35190057e9487378e937666a0c61f50d0bf8d2e5
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-24 04:42:34 +0600

    create 3 api for user

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ws4mpjc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,15 +25,23 @@ async function run() {
     const userCollection = client.db("domainHub").collection("users");
     // monjur code finish
 
-
     // Digontha Code start
-    const freeTrialUserCollection = client.db("domainHub").collection("freeTrialUsers")
+    const freeTrialUserCollection = client
+      .db("domainHub")
+      .collection("freeTrialUsers");
     // Digontha Code finish
 
-
-
-
     // monjur code start
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const cursor = { email: email };
+      const result = await userCollection.findOne(cursor);
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const email = req.body.email;
@@ -49,28 +55,42 @@ async function run() {
         res.send(result);
       }
     });
-    // monjur code finish
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const email = req.body.email;
+      const cursor = { email: email };
+      const updatedDoc = {
+        $set: {
+          name: user.name,
+          phone: user.phone,
+        },
+      };
+      const result = await userCollection.updateOne(cursor, updatedDoc);
+      res.send(result)
 
+    });
+
+    // monjur code finish
 
     //  Digontha Code start
     app.post("/freeTrialUsers", async (req, res) => {
       const user = req.body;
       console.log(user);
-      const result = await freeTrialUserCollection.insertOne(user)
+      const result = await freeTrialUserCollection.insertOne(user);
       res.send(result);
     });
 
     app.get("/freeTrialUsers", async (req, res) => {
       const email = req.query.email;
       console.log(email);
-      let query = {}
-      if(email){
-        query = { email: email};
+      let query = {};
+      if (email) {
+        query = { email: email };
       }
       const result = await freeTrialUserCollection.find(query).toArray();
-      res.send(result)
+      res.send(result);
     });
-   //  Digontha Code finish
+    //  Digontha Code finish
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -78,70 +98,67 @@ async function run() {
     const domainCollection = client.db("domainHub").collection("domain");
     const cartsCollection = client.db("domainHub").collection("carts");
 
-
     // domain related api//Abubakar
 
-    app.get('/domain', async (req, res) => {
+    app.get("/domain", async (req, res) => {
       const result = await domainCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-
-    app.get('/domain/:id', async (req, res) => {
+    app.get("/domain/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: (id) }
+      const query = { _id: id };
       const result = await domainCollection.findOne(query);
       res.send(result);
-    })
-    app.post('/domain', async (req, res) => {
+    });
+    app.post("/domain", async (req, res) => {
       const item = req.body;
       const result = await domainCollection.insertOne(item);
       res.send(result);
-    })
+    });
     // update domain
-    app.patch('/domain/:id', async (req, res) => {
+    app.patch("/domain/:id", async (req, res) => {
       const item = req.body;
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
+      const filter = { _id: new ObjectId(id) };
       console.log(filter);
       const updatedDoc = {
         $set: {
           name: item.name,
           category: item.category,
           price: item.price,
-        }
-      }
+        },
+      };
       // console.log(updatedDoc);
-      const result = await domainCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
+      const result = await domainCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
     // carts related api//Abubakar
-    app.get('/carts', async (req, res) => {
+    app.get("/carts", async (req, res) => {
       const result = await cartsCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/carts', async (req, res) => {
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       const result = await cartsCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.delete('/carts/:id', async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
       res.send(result);
-    })
-    app.delete('/carts/:id', async (req, res) => {
+    });
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = { _id: ObjectId(id) }
+      const query = { _id: ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
       res.send(result);
-    })
+    });
     // carts related api//Abubakar
-
 
     // await client.connect();
     // Send a ping to confirm a successful connection
